600a5dd598021502eeb6530761bd3102c88b6279
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2022-05-13 03:15:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // report
+            var report = new WorkReport();
+            report.AddEntry(new WorkReportEntry { ProjectCode = "123ABC", ProjectName = "Report Test1", SpentHours = 34 });
+            report.AddEntry(new WorkReportEntry { ProjectCode = "546BBB", ProjectName = "Report Test2", SpentHours = 22 });
+            Console.WriteLine(report.ToString());
+
+            // schedule task
+            var scheduler = new Scheduler();
+            scheduler.AddEntry(new ScheduleTask { TaskId = 1, Content = "Can you see me? I'm task 1", ExecuteOn = DateTime.Now.AddDays(1) });
+            scheduler.AddEntry(new ScheduleTask { TaskId = 2, Content = "Can you see me? I'm task 2", ExecuteOn = DateTime.Now.AddDays(4) });
+            Console.WriteLine(scheduler.ToString());
+
+            // check folder Bin => Debug => Reports folder and ScheduleTask folder
+            var saver = new FileSaver();
+            saver.SaveToFile(@"Reports", "WorkReport.txt", report);
+            saver.SaveToFile(@"ScheduleTask", "Schedule.txt", scheduler);
+        }
+    }
+}
---
+++ WorkReport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class WorkReport : IEntryManager<WorkReportEntry>
+    {
+        private readonly List<WorkReportEntry> _entries;
+
+        public WorkReport()
+        {
+            _entries = new List<WorkReportEntry>();
+        }
+
+        public void AddEntry(WorkReportEntry entry) => _entries.Add(entry);
+
+        public void RemoveEntryAt(int index) => _entries.RemoveAt(index);
+
+        // we remove below commented code to separated file to apply SRP
+        #region Refactored method
+        //public void SaveToFile(string directoryPath,string fileName)
+        //{
+        //    if (!Directory.Exists(directoryPath))
+        //    {
+        //        Directory.CreateDirectory(directoryPath);
+        //    }
+        //    File.WriteAllText(Path.Combine(directoryPath, fileName), ToString());
+        //}
+        #endregion
+        public override string ToString() => string.Join(Environment.NewLine, _entries.Select(e => $"Code: {e.ProjectCode},Name: {e.ProjectName}, Hours:{e.SpentHours}"));
+    }
+}
